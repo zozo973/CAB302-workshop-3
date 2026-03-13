9ef9695232743d3069d1a3fc747262f64e4fca58
Hugo Powe <11580178@qut.edu.au>
2026-03-13 13:02:27 +1000

    Set up project views, read for activity 1.1

--- src/main/java/com/example/addressbook/HelloController.java
+++ src/main/java/com/example/addressbook/HelloController.java
@@ -2,10 +2,33 @@ package com.example.addressbook;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+        @FXML
+        private Label welcomeText;
+        @FXML
+        private TextArea termsAndConditions;
+
+        @FXML
+        public void initialize() {
+            termsAndConditions.setText("""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Eget dolor morbi non arcu risus. Quis lectus nulla at volutpat diam
+ut venenatis tellus in. Feugiat in fermentum posuere urna nec tincidunt
+praesent semper. Turpis tincidunt id aliquet risus feugiat in.
+Libero volutpat sed cras ornare. Facilisi morbi tempus iaculis urna.
+Bibendum est ultricies integer quis auctor. Eu augue ut lectus arcu.
+Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.
+Gravida neque convallis a cras. Elit ut aliquam purus sit.
+Suspendisse ultrices gravida dictum fusce ut placerat.
+Integer feugiat scelerisque varius morbi enim nunc.
+Amet justo donec enim diam vulputate ut pharetra.
+Sapien pellentesque habitant morbi tristique.
+Lorem sed risus ultricies tristique nulla aliquet.
+Elementum nibh tellus molestie nunc non blandit massa.""");
+        }
 
     @FXML
     protected void onHelloButtonClick() {
