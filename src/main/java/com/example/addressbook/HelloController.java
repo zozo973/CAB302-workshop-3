f157adb65fd25fed7d68299bc5b92a9064213e13
Hugo Powe <11580178@qut.edu.au>
2026-03-13 13:08:32 +1000

    Added initalize FXML method for setting text for termsAndConditions

--- src/main/java/com/example/addressbook/HelloController.java
+++ src/main/java/com/example/addressbook/HelloController.java
@@ -17,6 +17,26 @@ public class HelloController {
     private Button nextButton;
 
     @FXML
+    public void initialize() {
+        termsAndConditions.setText("""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Eget dolor morbi non arcu risus. Quis lectus nulla at volutpat diam
+ut venenatis tellus in. Feugiat in fermentum posuere urna nec tincidunt
+praesent semper. Turpis tincidunt id aliquet risus feugiat in.
+Libero volutpat sed cras ornare. Facilisi morbi tempus iaculis urna.
+Bibendum est ultricies integer quis auctor. Eu augue ut lectus arcu.
+Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.
+Gravida neque convallis a cras. Elit ut aliquam purus sit.
+Suspendisse ultrices gravida dictum fusce ut placerat.
+Integer feugiat scelerisque varius morbi enim nunc.
+Amet justo donec enim diam vulputate ut pharetra.
+Sapien pellentesque habitant morbi tristique.
+Lorem sed risus ultricies tristique nulla aliquet.
+Elementum nibh tellus molestie nunc non blandit massa.""");
+    }
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
