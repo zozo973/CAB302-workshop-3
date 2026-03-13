7be34919733d38c0c01f2301636500fa73000b70
Hugo Powe <11580178@qut.edu.au>
2026-03-13 12:59:01 +1000

    Added agreeCheckBox onAction functionality undisabling Next button (activity 1.1)

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -8,11 +8,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static final String TITLE = "Address Book";
+    private static final int WIDTH = 640;
+    private static final int HEIGHT =360;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/addressbook/HelloController.java
+++ src/main/java/com/example/addressbook/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.addressbook;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
     @FXML
+    private TextArea termsAndConditions;
+    @FXML
     private Label welcomeText;
+    @FXML
+    private CheckBox agreeCheckBox;
+    @FXML
+    private Button nextButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onAgreeCheckBoxClick() {
+        boolean accepted = agreeCheckBox.isSelected();
+        nextButton.setDisable(!accepted);
     }
 }
