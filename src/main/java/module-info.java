b88a727acbac1ad38de28887963e17258cbb22eb
Hugo Powe <11580178@qut.edu.au>
2026-03-13 12:15:41 +1000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.addressbook {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.addressbook to javafx.fxml;
+    exports com.example.addressbook;
+}
